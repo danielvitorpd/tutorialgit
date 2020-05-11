7d65997d0bbc98fbc04a2b6ca5b455b37085d8e3
danielvitorpd <danielvitor@usp.br>
2020-05-11 15:19:07 -0300

    first commit :ok:

---
+++ tutorial.py
@@ -0,0 +1,16 @@
+class Tutorial:
+
+    def __init__(self, branch):
+        self.__branch = branch
+
+    @property
+    def branch(self):
+        return self.__branch
+
+    @branch.setter
+    def branch(self, value):
+        self.__branch = value
+
+
+master = Tutorial('master')
+print(master.branch)
